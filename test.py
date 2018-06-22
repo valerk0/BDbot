dfa8495497a0bf482b48b208744cd5f43e6e125d
V <valera.arh@gmail.com>
2018-06-22 17:45:48 +0300

    1

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from tstobj import message, chat, user
 
 cht=chat(12345678,'some chat')
 usr=user(98765432, 'uname', 'name')
-msg=message(chat,user)
+msg=message(cht,usr)
 
 bdy=BDayObj(usr)
 bdy.SetDate('/setdate 25.05.1987')
