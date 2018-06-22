3804edbc1eb68ed1430c1b700b6d1668baae9d7d
V <valera.arh@gmail.com>
2018-06-22 18:56:32 +0300

    1

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ from BDayObj import BDayObj
 from tstobj import message, chat, user
 
 cht=chat(12345678,'some chat')
-usr=user(98765432, 'uname', 'name')
+usr=user(9876543, 'uname', 'name')
 msg=message(cht,usr)
 
 bdy=BDayObj(usr)
 bdy.SetDate('/setdate 25.05.1987')
 
 db=DB()
-db.HandleMsg(msg)
+#db.HandleMsg(msg)
 db.SaveBDay(bdy)
 print(','.join('%s:%s' % i for i in vars(bdy).items()))
