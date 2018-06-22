615372d1f3d49786df26f092f99627e3e1049eb8
V <valera.arh@gmail.com>
2018-06-22 14:33:01 +0300

    1

--- DropTbls.py
+++ DropTbls.py
@@ -5,8 +5,8 @@ from DBconfData import DBconf
 
 with psycopg2.connect(**DBconf.db_params) as conn:
     curs=conn.cursor()
-    curs.execute(drop table cht;)
+    curs.execute('drop table cht;')
 
-    curs.execute(drop table usr;)
+    curs.execute('drop table usr;')
 
-    curs.execute(drop table chtusr;)
+    curs.execute('drop table chtusr;')
