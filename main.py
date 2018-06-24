4d82151ca03864fb52f19aebd586fb8077c5facb
V <valera.arh@gmail.com>
2018-06-24 20:15:20 +0300

    1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from emoji import emojize
 @private
 def start(bot, update):
     update.message.reply_text('Пожалуйста, задайте дату рождения в формате:\n/bday dd.mm.yyyy')
+    print('start')
 
 @private
 def help(bot, update):
@@ -18,6 +19,7 @@ def help(bot, update):
         Пожалуйста, задайте дату рождения в формате:\n
         /bday dd.mm.yyyy
         '''))
+    print('help')
 
 def setBDay(bot, update, args):
     if ~len(args)==1|(len(args)==1&~len(args[0].split('.'))==3):
@@ -26,16 +28,19 @@ def setBDay(bot, update, args):
             Правильный формат:\n
             /bday dd.mm.yyyy
             '''))
+        print('bday fail')
         return
     bday=BDayObj(update.message.from_user)
     bday.SetDate(args[0])
     db=DB()
     db.SaveBDay(bday)
     update.message.reply_text(emojize('Сохранил:thumbs up:'))
+    print('bday success')
 
 def ProcessMsg(bot, update):
     db=DB()
     db.HandleMsg(update)
+    print('process msg')
 
 def DaylyJob(bot, job):
     pass
@@ -44,6 +49,7 @@ def MonthlyJob(bot, job):
     pass
 
 def main():
+    print('start program')
     conf=confData('botconf.ini','BOT')
 
     updater=Updater(**conf.params)
@@ -54,10 +60,13 @@ def main():
     dp.add_handler(CommandHandler('start', start),1)
     dp.add_handler(CommandHandler('help', help),1)
     dp.add_handler(CommandHandler('bday', setBDay, pass_args=True),1)
+    print('handlers added')
 
     dJob=jq.run_repeating(DaylyJob, DayInterval, FirstDay)
     mJob=jq.run_once(MonthlyJob, NextMonth)
+    print('jobs added')
 
     updater.start_polling()
     updater.idle()
+    print('-1')
 
