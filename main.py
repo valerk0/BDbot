7add84020adbc1b2a94576bbbdd54f00b81b2fb3
V <valera.arh@gmail.com>
2018-06-26 22:15:59 +0300

    1

--- main.py
+++ main.py
@@ -96,7 +96,6 @@ def main():
     print('handlers added')
 
     dJob=jq.run_repeating(DaylyJob, DayInterval(), FirstDay())
-    dJob=jq.run_repeating(DaylyJob, 10, 1)
     print('jobs added')
 
     updater.start_polling()
