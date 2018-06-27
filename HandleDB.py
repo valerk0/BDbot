72a0082035973ec2b597bee1722bc7132e825d8f
V <valera.arh@gmail.com>
2018-06-27 20:02:26 +0300

    1

--- HandleDB.py
+++ HandleDB.py
@@ -8,7 +8,6 @@ class DB(object):
     def __init__(self):
         conf=confData('dbconfig.ini','DATABASE')
         self.__conn=psycopg2.connect(**conf.params)
-        self.__curs=self.__conn.cursor()
 
     def __del__(self):
         self.__conn.close()
@@ -18,60 +17,58 @@ class DB(object):
         usr=upd.effective_user
         cht=upd.effective_chat
 
-        try:
-            self.__curs.execute('''
-                insert into usr values ({0:d}, '{1:s}', '{2:s}');
-            '''.format(usr.id, usr.username.strip(), usr.first_name.strip()))
-            self.__conn.commit()
-        except:
-            pass
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    insert into usr values ({0:d}, '{1:s}', '{2:s}');
+                '''.format(usr.id, usr.username.strip(), usr.first_name.strip()))
 
         if cht.id==usr.id:
             return
 
-        self.__conn.close()
-        conf=confData('dbconfig.ini','DATABASE')
-        self.__conn=psycopg2.connect(**conf.params)
-        self.__curs=self.__conn.cursor()
-        try:
-            self.__curs.execute(u"insert into cht values ({0:d}, '{1:s}');".format(cht.id, cht.title.strip()))
-            self.__conn.commit()
-        except:
-            pass
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    insert into cht values ({0:d}, '{1:s}');
+                '''.format(cht.id, cht.title.strip()))
 
-        self.__conn.close()
-        conf=confData('dbconfig.ini','DATABASE')
-        self.__conn=psycopg2.connect(**conf.params)
-        self.__curs=self.__conn.cursor()
-        try:
-            self.__curs.execute('''
-                insert into chtusr values ('{0:d}_{1:d}', {2:d}, {3:d});
-            '''.format(cht.id, usr.id, cht.id, usr.id))
-            self.__conn.commit()
-        except:
-            pass
-
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    insert into chtusr values ('{0:d}_{1:d}', {2:d}, {3:d});
+                '''.format(cht.id, usr.id, cht.id, usr.id))
 
 
     def SaveBDay(self, bday):
-        self.__curs.execute('select uid from usr where uid={0:d}'.format(bday.uid))
-        if self.__curs.fetchall().__len__()>0:
-            self.__curs.execute('''
-               update usr set bd={0:d}, bm={1:d}, by={2:d}
-                where uid={3:d}
-            '''.format(bday.bd, bday.bm, bday.by, bday.uid))
-        else:
-            self.__curs.execute('''
-                insert into usr values ({0:d}, '{1:s}', '{2:s}', {3:d}, {4:d}, {5:d});
-            '''.format(bday.uid, bday.uuname, bday.uname, bday.bd, bday.bm, bday.by))
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    select uid from usr where uid={0:d};
+                '''.format(bday.uid))
+                isexist=curs.fetchall()
 
-        self.__conn.commit()
+        if isexist.__len__()>0:
+            with self.__conn as conn:
+                with conn.cursor() as curs:
+                    curs.execute('''
+                        update usr set bd={0:d}, bm={1:d}, by={2:d} where uid={3:d};
+                    '''.format(bday.bd, bday.bm, bday.by, bday.uid))
+        else:
+            with self.__conn as conn:
+                with conn.cursor() as curs:
+                    curs.execute('''
+                        insert into usr values ({0:d}, '{1:s}', '{2:s}', {3:d}, {4:d}, {5:d});
+                    '''.format(bday.uid, bday.uuname, bday.uname, bday.bd, bday.bm, bday.by))
+
 
     def getDateByUN(self, UN):
-        self.__curs.execute('''
-                select * from usr where uuname='{0:s}'
-            '''.format(UN))
-        rows=self.__curs.fetchall()
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    select * from usr where uuname='{0:s};
+                '''.format(UN))
+                rows=curs.fetchall()
+
         if rows.__len__()>0:
             records=rows[0]
             if not records[3]:
@@ -79,19 +76,28 @@ class DB(object):
             else:
                 return datetime.date(year=records[5], month=records[4], day=records[3])
         else:
-            return 
+            return False
+
 
     def GetUserBDayList(self):
-        now=datetime.datetime.now()
-        self.__curs.execute('select uid, uuname, uname from usr where bd={0:d} and bm={1:d}' \
-                            .format(now.day, now.month))
-        return self.__curs.fetchall()
+        with self.__conn as conn:
+            with conn.cursor() as curs:
+                curs.execute('''
+                    select uid, uuname, uname from usr where bd={0:d} and bm={1:d};
+                '''.format(now.day, now.month))
+                return curs.fetchall()
+
 
     def GetChatsList(self,userList):
         results=[]
         for user in userList:
-            self.__curs.execute('select cid from chtusr where uid={0:d}'.format(user[0]))
-            chatsrows=self.__curs.fetchall()
+            with self.__conn as conn:
+                with conn.cursor() as curs:
+                    curs.execute('''
+                        select cid from chtusr where uid={0:d};
+                    '''.format(user[0]))
+                    chatsrows=self.__curs.fetchall()
+
             result=[]
             if len(chatsrows)>0:
                 for chats in chatsrows:
@@ -101,4 +107,3 @@ class DB(object):
                 result.append(user[0])
             results.append(result)
         return results
-
