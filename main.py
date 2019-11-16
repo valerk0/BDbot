b7cf9ec03406e902955989203f0f95af3704face
valerk0 <valerk0@users.noreply.github.com>
2019-11-16 10:37:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,8 +68,9 @@ def getBDay(bot,update,args):
 
 def get10(bot,update):
 	db=DB()
-	bot.send_message(chat_id='@BabaR0bot', text='get10')
+	bot.send_message(chat_id=update.effective_chat.id, text='get10')
 	usrs=db.get10(update)
+	bot.send_message(chat_id=update.effective_chat.id, text=usrs)
 	if usrs:
 		txt='Ближайшие дни рождения:\n'
 		for n,x in usrs:
