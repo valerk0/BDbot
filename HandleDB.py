afe2aa4ccec654d5b1194ccda2ba325455fd3c37
Valery Dulov <valera.arh@gmail.com>
2021-06-30 18:20:46 +0300

    Another fix for users update

--- HandleDB.py
+++ HandleDB.py
@@ -44,6 +44,7 @@ class DB(object):
                                 fulln = '{} {}'.format(fname, lname)
                             else:
                                 fulln = fname or lname
+                            print(usr.id, usr.username.strip(), fulln)
                             curs.execute('''
                                 update usr set uuname='{1:s}', uname='{2:s}' where uid={0:d};
                             '''.format(usr.id, usr.username.strip(), fulln))
